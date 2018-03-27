e83c1089eb832edecbcd1f367f5eeb140cf34794
rodolfoip <rodolfo.pereirailce@gmail.com>
2018-03-27 19:08:29 -0300

    Added screen account

---
+++ src/view/Account.java
@@ -0,0 +1,12 @@
+package view;
+
+import javax.swing.*;
+
+public class Account {
+    private JPanel panel1;
+    private JTable tableCoins;
+    private JButton comprarMoedasButton;
+    private JButton venderMoedasButton;
+    private JButton depositarButton;
+    private JButton sacarButton;
+}
