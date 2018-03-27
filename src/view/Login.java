4bf2bfc642f45428b9ea4cce51e4498198bc60de
rodolfip <rodolfo.pereirailce@gmail.com>
2018-03-27 09:22:47 -0300

    Added view login

---
+++ src/view/Login.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.*;
+
+public class Login {
+    private JPanel panelContent;
+    private JTextField loginField;
+    private JPasswordField passwordField;
+    private JButton entrarButton;
+    private JButton criarContaButton;
+}
