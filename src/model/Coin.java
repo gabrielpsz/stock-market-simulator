3c061b82b045ac2691482c268fc7cf0aa60a8a61
Marcelo Brosowicz de Paulo <mbrosowicz@hotmail.com>
2018-03-29 14:13:34 -0300

    Creating model methods

--- src/model/Coin.java
+++ src/model/Coin.java
@@ -5,4 +5,19 @@ public class Coin {
     private String name;
     private float price;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
--- src/model/User.java
+++ src/model/User.java
@@ -13,4 +13,43 @@ public class User {
 //    private Map<String, float> wallet;
 
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
