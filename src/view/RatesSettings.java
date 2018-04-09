1379b78e7b8a96747f762b05ab59dec229d4185d
rodolfoip <rodolfo.pereirailce@gmail.com>
2018-04-09 09:58:12 -0300

    added new screen for rates settings

---
+++ src/view/RatesSettings.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class RatesSettings extends Application {
+
+    private static Stage stage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent sellCoin = FXMLLoader.load(getClass().getResource("RatesSettings.fxml"));
+        Scene scene = new Scene(sellCoin);
+        stage.setScene(scene);
+        stage.show();
+        setStage(stage);
+    }
+
+    public static Stage getStage() {
+        return stage;
+    }
+
+    public static void setStage(Stage stage) {
+        RatesSettings.stage = stage;
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
